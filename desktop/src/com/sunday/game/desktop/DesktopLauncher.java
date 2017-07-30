890318d2b37fb584d8bdc8dc4cfe3137d2e3a006
Hrirks <Hrirks10@gmail.com>
2017-07-30 20:28:30 +0200

    Desktop Launcher Demo

--- desktop/src/com/sunday/game/desktop/DesktopLauncher.java
+++ desktop/src/com/sunday/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.sunday.game.SundayMain;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Sunday Game";
+        config.useGL30 = true;
+        config.width = 480;
+        config.height = 320;
+
         new LwjglApplication(new SundayMain(), config);
     }
 }
