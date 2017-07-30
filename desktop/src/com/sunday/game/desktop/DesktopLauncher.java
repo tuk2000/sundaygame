b2f4dae8e2dac0f3e2f61dca8f76ab5faa510ce2
youngandwell <youngandwell@163.com>
2017-07-30 18:46:27 +0200

    inital Project Upload

---
+++ desktop/src/com/sunday/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sunday.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sunday.game.SundayMain;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new SundayMain(), config);
+    }
+}
