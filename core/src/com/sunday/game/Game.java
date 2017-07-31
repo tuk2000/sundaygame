b630da1c47fefeab2b82a8a8236104f2f0c3d88d
Hrirks <Hrirks10@gmail.com>
2017-07-31 08:59:38 +0200

    rect - runners platform and box

--- core/src/com/sunday/game/Game.java
+++ core/src/com/sunday/game/Game.java
@@ -6,15 +6,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.*;
+import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled;
 
 public class Game implements ApplicationListener{
-    private SpriteBatch batch;
-    private BitmapFont font;
+     SpriteBatch batch;
+     BitmapFont font;
+     ShapeRenderer shapeRenderer;
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
@@ -26,6 +32,11 @@ public class Game implements ApplicationListener{
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.rect(20,20,20,20);
+        shapeRenderer.rect(0,0,800,20,Color.BLACK,Color.GRAY,Color.DARK_GRAY,Color.LIGHT_GRAY);
+        shapeRenderer.end();
         batch.begin();
         font.draw(batch,"Hello World",200,200);
         batch.end();
@@ -45,5 +56,6 @@ public class Game implements ApplicationListener{
     public void dispose() {
         batch.dispose();
         font.dispose();
+        shapeRenderer.dispose();
     }
 }
