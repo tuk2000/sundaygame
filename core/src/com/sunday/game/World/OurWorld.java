1909b53d3be7420b43d24b5558671e8269fdb86b
Hrirks <Hrirks10@gmail.com>
2017-08-01 21:22:51 +0200

    Organisierte Klasse

--- core/src/com/sunday/game/World/OurWorld.java
+++ core/src/com/sunday/game/World/OurWorld.java
@@ -1,8 +1,4 @@
 package com.sunday.game.World;
 
-import com.badlogic.gdx.math.Vector2;
-
-public class OurWorld {
-    //Ich habe die Klasse World zu OurWorld umbennen , weil es noch eine classe mit gleicher Name entsteht
-    
+public interface OurWorld {
 }
