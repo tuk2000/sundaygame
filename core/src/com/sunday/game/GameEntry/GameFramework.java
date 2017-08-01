3e692499c08e7e303449d26d871e4526238009ba
youngandwell <youngandwell@163.com>
2017-08-01 22:17:29 +0200

    adjust Interfaces and extends game framework

---
+++ core/src/com/sunday/game/GameEntry/GameFramework.java
@@ -0,0 +1,11 @@
+package com.sunday.game.GameEntry;
+
+import com.sunday.game.Graphic.GraphicPresentation;
+import com.sunday.game.UserInput.UserInputManager;
+import com.sunday.game.World.GameWorld;
+
+public class GameFramework {
+    public static GraphicPresentation graphic;
+    public static GameWorld gameWorld;
+    public static UserInputManager inputManager;
+}
--- core/src/com/sunday/game/World/GameWorld.java
+++ core/src/com/sunday/game/World/GameWorld.java
@@ -1,4 +1,4 @@
 package com.sunday.game.World;
 
-public interface OurWorld {
+public interface GameWorld {
 }
