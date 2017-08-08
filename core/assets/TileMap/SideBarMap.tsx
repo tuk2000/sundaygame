c0bcec9672495f9756840b34bd4ba9987268c44c
Hrirks <Hrirks10@gmail.com>
2017-08-08 17:04:12 +0200

    Code Fix with Tiled map Test

---
+++ core/assets/TileMap/SideBarMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SideBarRock" tilewidth="50" tileheight="50" tilecount="81" columns="9">
+ <image source="stone wall 8.png" width="480" height="480"/>
+</tileset>
