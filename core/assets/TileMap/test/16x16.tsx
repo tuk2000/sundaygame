b2915e507bada3b4d402c5f857cb65875e5c2b6b
Hrirks <Hrirks10@gmail.com>
2017-08-08 20:49:50 +0200

    Code Fix with Tiled

---
+++ core/assets/TileMap/test/16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sano16x16" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="tile roof 1.png" width="480" height="480"/>
+</tileset>
